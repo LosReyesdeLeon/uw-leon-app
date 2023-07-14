a99919d1aa7cda9a6ca6605d5d9f82f1a3eaff3c
Devin <devin.peevy@outlook.com>
2023-07-14 10:56:21 -0700

    Added some strings and a VERY BASIC home page, so that we can navigate to other activities. Made a NavigationPage and AboutLeonPage so that group 1 can start working on their parts

--- app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/HomePage.java
+++ app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/HomePage.java
@@ -2,13 +2,36 @@ package edu.tacoma.uw.set.css.dpeevy.uwleonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Button b = findViewById(R.id.about_button);
+        setButtonListener(b, AboutLeonPage.class);
+
+        b = findViewById(R.id.navigation_button);
+        setButtonListener(b, NavigationPage.class);
+    }
+
+    private void setButtonListener(Button button, Class<?> cls) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), cls);
+                startActivity(i);
+            }
+        });
     }
 }
