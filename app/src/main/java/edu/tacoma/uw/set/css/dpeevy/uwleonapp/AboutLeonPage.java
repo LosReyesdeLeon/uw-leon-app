f6871ef786ae2abfcca029ea6906ca7bc0223884
Devin <devin.peevy@outlook.com>
2023-07-14 10:56:21 -0700

    Added some strings and a VERY BASIC home page, so that we can navigate to other activities. Made a NavigationPage and AboutLeonPage so that group 1 can start working on their parts

---
+++ app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/AboutLeonPage.java
@@ -0,0 +1,14 @@
+package edu.tacoma.uw.set.css.dpeevy.uwleonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutLeonPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_leon_page);
+    }
+}
