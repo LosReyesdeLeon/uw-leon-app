21ead98c13b32d34ebb0d5c8d057ce02cae8d013
tommy <nat.nguyen2001@gmail.com>
2023-07-17 09:05:51 -0700

    I made the about page for points of interests

--- app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/AboutLeonPage.java
+++ app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/AboutLeonPage.java
@@ -3,12 +3,25 @@ package edu.tacoma.uw.set.css.dpeevy.uwleonapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutLeonPage extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.about_leon_page);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_leon_page);
-    }
+            TextView textView = (TextView) findViewById(R.id.about);
+            textView.setText("Welcome to the About page\n" +
+                    "This is the About Page.\n" +
+                    "     \tHighlights:\n" +
+                    "     \tArt History\n" +
+                    "           \t-Architecture\n" +
+                    "           \t-Catholic Art\n" +
+                    "     \tIntellectual History\n" +
+                    "           \t-Astronauts\n" +
+                    "           \t-Cyber Security Center\n" +
+                    "     \tContemporary\n" +
+                    "           \t-Bars and parties"); //set text for text view
+        }
 }
