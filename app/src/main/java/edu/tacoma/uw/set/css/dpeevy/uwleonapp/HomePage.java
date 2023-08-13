03ff890636c359fea81dc6e28ab0926c39cd926c
satinder <satinder.10@hotmail.com>
2023-08-13 07:44:46 -0700

    lets other's add their views/activities

--- app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/HomePage.java
+++ app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/HomePage.java
@@ -6,32 +6,63 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomePage extends AppCompatActivity {
 
+    private ImageButton b1;
+    private ImageButton b2;
+    private ImageButton b3;
+    private ImageButton b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        // Inflate the ImageButtons
+        b1 = findViewById(R.id.imageButton);
+        b2 = findViewById(R.id.imageButton2);
+        b3 = findViewById(R.id.imageButton3);
+        b4 = findViewById(R.id.imageButton4);
+
+        // Set onClickListener for imageButton to start DesiredActivity
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, DesiredActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set onClickListener for imageButton2 to start AnotherActivity
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, AnotherActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Set onClickListener for imageButton to start DesiredActivity
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, DesiredActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Set onClickListener for imageButton to start DesiredActivity
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, DesiredActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-//        Button b = findViewById(R.id.about_button);
-//        setButtonListener(b, AboutLeonPage.class);
-//
-//        b = findViewById(R.id.navigation_button);
-//        setButtonListener(b, NavigationPage.class);
     }
 
-//    private void setButtonListener(Button button, Class<?> cls) {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getApplicationContext(), cls);
-//                startActivity(i);
-//            }
-//        });
-//    }
 }
